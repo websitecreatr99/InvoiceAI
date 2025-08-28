8d312e802beff5943e042fb1e7b10ff6be922de7
Kishan Pahadiya <kishanvir4321@gmail.com>
2025-08-28 12:08:05 +0530

    up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Image Data Extraction',
+    name = 'Invoice Extractor from unstructured PDF data',
     version= '0.0.0',
     author= 'Kishan Pahadiya',
     author_email= 'kishanvir4321@gmail.com',
