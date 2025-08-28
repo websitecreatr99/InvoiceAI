19b1cfc38045024a2005b66097d26a27b470541c
Kishan Pahadiya <kishanvir4321@gmail.com>
2025-08-28 12:46:54 +0530

    up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Invoice Extractor from unstructured PDF data',
+    name = 'InvoiceAI',
     version= '0.0.0',
     author= 'Kishan Pahadiya',
     author_email= 'kishanvir4321@gmail.com',
